d8e33dabb870aaabf865bfa3b4c93e234aef92c6
nhl1090 <neilhdz24@gmail.com>
2024-06-17 20:34:29 -0600

    Additional changes to js file.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -92,7 +92,13 @@ function handleDrop(event, ui) {
     renderTaskList();
   }
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+// Change made -- When the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
-});
+    renderTaskList();
+    $('#taskForm').on('submit', handleAddTask)
+  
+    $('.lane').droppable({
+      accept: '.card',
+      drop: handleDrop
+    });
+  });
